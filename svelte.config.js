f1da52f0c1629900e71d357171bea027c8219c05
Hasnat4763 <abulhasnat5737@gmail.com>
2025-12-25 21:19:13 +0600

    Configure for GitHub Pages user site

--- svelte.config.js
+++ svelte.config.js
@@ -5,8 +5,13 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 const config = {
 	preprocess: vitePreprocess(),
 
-	kit: { 
-		adapter: adapter()
+	kit: {
+		adapter: adapter({
+			fallback: '404.html'
+		}),
+		paths: {
+			base: ''
+		}
 	}
 };
 
